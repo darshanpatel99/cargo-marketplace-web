ce15489f6c8b1a7261f013e420a838c78411a117
gagandeepbajwa[C <gagandeepbajwa1998@gmail.com>
2019-12-19 17:43:16 -0800

    Removed Lazy Load and passed the product id in the url

--- src/views/Home/Sections/SectionProductCard.js
+++ src/views/Home/Sections/SectionProductCard.js
@@ -45,7 +45,7 @@ export default function Cards(props) {
     <CardBody>
     <h4 className={classes.cardTitle}>{props.title}</h4>
     <p>{props.description}</p>
-    <Link to={{pathname:'/product', state:safeStringify(item)}} >
+    <Link to={{pathname:`/product/${item.key}`, state:safeStringify(item)}} >
       <Button color="primary">Buy</Button>, 
     </Link>
       <Button color="primary">Chat</Button>
